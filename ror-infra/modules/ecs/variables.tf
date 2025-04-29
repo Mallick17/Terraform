bcdba74e487f25005f0c166d8be7fc829142e057
Gyanaranjan Mallick <gyanaranjanmallick444@gmail.com>
2025-04-29 23:44:38 +0530

    Update variables.tf

--- ror-infra/modules/ecs/variables.tf
+++ ror-infra/modules/ecs/variables.tf
@@ -1,14 +1,23 @@
 variable "cluster_name" {}
+variable "instance_type" {}
+variable "asg_min" {}
+variable "asg_max" {}
+variable "asg_desired" {}
+variable "key_pair" {}
+variable "instance_profile_name" {}
+variable "subnet_ids" { type = list(string) }
+variable "sg_ids" { type = list(string) }
+
+# Task config
 variable "task_family" {}
-variable "service_name" {}
-variable "task_execution_role_arn" {}
+variable "execution_role_arn" {}
 variable "app_image" {}
 
+# App env vars
 variable "db_user" {}
 variable "db_password" {}
 variable "db_host" {}
-variable "db_port" {}
 variable "db_name" {}
-variable "redis_url" {}
 variable "rails_master_key" {}
 variable "secret_key_base" {}
+variable "ebs_volume_size" { default = 30 }
