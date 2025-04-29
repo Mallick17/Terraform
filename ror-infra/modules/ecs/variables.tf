52f11c9b134128e6fdf67f61501aff1c508735ac
Gyanaranjan Mallick <gyanaranjanmallick444@gmail.com>
2025-04-29 23:41:27 +0530

    Create variables.tf

---
+++ ror-infra/modules/ecs/variables.tf
@@ -0,0 +1,14 @@
+variable "cluster_name" {}
+variable "task_family" {}
+variable "service_name" {}
+variable "task_execution_role_arn" {}
+variable "app_image" {}
+
+variable "db_user" {}
+variable "db_password" {}
+variable "db_host" {}
+variable "db_port" {}
+variable "db_name" {}
+variable "redis_url" {}
+variable "rails_master_key" {}
+variable "secret_key_base" {}
